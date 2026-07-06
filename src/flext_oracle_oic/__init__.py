a258184508d56f11480899dcd21c7953d669a164
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-05 22:37:09 -0300

    fixes

--- src/flext_oracle_oic/__init__.py
+++ src/flext_oracle_oic/__init__.py
@@ -19,7 +19,13 @@ from flext_oracle_oic.__version__ import (
 from flext_oracle_oic._exports import FLEXT_ORACLE_OIC_LAZY_IMPORTS
 
 if TYPE_CHECKING:
-    from flext_core._root_typing_parts import d as d, e as e, h as h, r as r, x as x
+    from flext_core._root_typing_parts.facades import (
+        d as d,
+        e as e,
+        h as h,
+        r as r,
+        x as x,
+    )
     from flext_oracle_oic.api import (
         FlextOracleOicApi as FlextOracleOicApi,
         oracle_oic as oracle_oic,
--- src/flext_oracle_oic/_exports.py
+++ src/flext_oracle_oic/_exports.py
@@ -3,12 +3,60 @@
 
 from __future__ import annotations
 
-from flext_core.lazy import merge_lazy_imports
-from flext_oracle_oic._exports_lazy_part_01 import FLEXT_ORACLE_OIC_LAZY_IMPORTS_PART_01
+from flext_core.lazy import build_lazy_import_map, merge_lazy_imports
 
-_LOCAL_LAZY_IMPORTS = {
-    **FLEXT_ORACLE_OIC_LAZY_IMPORTS_PART_01,
-}
+_LOCAL_LAZY_IMPORTS = build_lazy_import_map(
+    {
+        "._utilities": ("_utilities",),
+        ".api": (
+            "FlextOracleOicApi",
+            "oracle_oic",
+        ),
+        ".constants": (
+            "FlextOracleOicConstants",
+            "c",
+        ),
+        ".main": (
+            "FlextOracleOicCli",
+            "main",
+        ),
+        ".models": (
+            "FlextOracleOicModels",
+            "m",
+        ),
+        ".protocols": (
+            "FlextOracleOicProtocols",
+            "p",
+        ),
+        ".service": (
+            "FlextOracleOicService",
+            "s",
+        ),
+        ".services": ("services",),
+        ".services.auth": ("FlextOracleOicAuthMixin",),
+        ".services.base": ("FlextOracleOicServiceBase",),
+        ".services.integration_crud": ("FlextOracleOicIntegrationCrudMixin",),
+        ".services.integration_lifecycle": ("FlextOracleOicIntegrationLifecycleMixin",),
+        ".services.monitoring": ("FlextOracleOicMonitoringMixin",),
+        ".services.orchestration": ("FlextOracleOicOrchestrationMixin",),
+        ".settings": ("FlextOracleOicSettings",),
+        ".typings": (
+            "FlextOracleOicTypes",
+            "t",
+        ),
+        ".utilities": (
+            "FlextOracleOicUtilities",
+            "u",
+        ),
+        "flext_core._root_typing_parts.facades": (
+            "d",
+            "e",
+            "h",
+            "r",
+            "x",
+        ),
+    },
+)
 
 FLEXT_ORACLE_OIC_LAZY_IMPORTS = merge_lazy_imports(
     (".services",),
