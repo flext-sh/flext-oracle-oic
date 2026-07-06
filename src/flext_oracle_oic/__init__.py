6c5740d55fdcae498910150a0442fc13ed3856cd
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-06 02:46:40 -0300

    chore(codegen): inline lazy __init__.py, eliminate _exports.py sidecar
    Regenerated by 'flext_infra codegen init --apply': PEP 562 lazy imports are
    now inlined directly into each package __init__.py; the separate _exports.py
    lazy-map sidecar is removed (superseded by the inline map).

--- src/flext_oracle_oic/__init__.py
+++ src/flext_oracle_oic/__init__.py
@@ -5,7 +5,11 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from flext_core.lazy import install_lazy_exports
+from flext_core.lazy import (
+    build_lazy_import_map,
+    install_lazy_exports,
+    merge_lazy_imports,
+)
 from flext_oracle_oic.__version__ import (
     __author__,
     __author_email__,
@@ -16,119 +20,105 @@ from flext_oracle_oic.__version__ import (
     __version__,
     __version_info__,
 )
-from flext_oracle_oic._exports import FLEXT_ORACLE_OIC_LAZY_IMPORTS
 
 if TYPE_CHECKING:
-    from flext_auth import d as d, e as e, h as h, r as r, x as x
+    from flext_auth import d, e, h, r, x
 
-    from flext_oracle_oic.api import (
-        FlextOracleOicApi as FlextOracleOicApi,
-        oracle_oic as oracle_oic,
-    )
-    from flext_oracle_oic.constants import (
-        FlextOracleOicConstants as FlextOracleOicConstants,
-        c as c,
-    )
-    from flext_oracle_oic.main import (
-        FlextOracleOicCli as FlextOracleOicCli,
-        main as main,
-    )
-    from flext_oracle_oic.models import (
-        FlextOracleOicModels as FlextOracleOicModels,
-        m as m,
-    )
-    from flext_oracle_oic.protocols import (
-        FlextOracleOicProtocols as FlextOracleOicProtocols,
-        p as p,
-    )
-    from flext_oracle_oic.service import (
-        FlextOracleOicService as FlextOracleOicService,
-        s as s,
-    )
-    from flext_oracle_oic.services.auth import (
-        FlextOracleOicAuthMixin as FlextOracleOicAuthMixin,
-    )
-    from flext_oracle_oic.services.base import (
-        FlextOracleOicServiceBase as FlextOracleOicServiceBase,
-    )
+    from flext_oracle_oic.api import FlextOracleOicApi
+    from flext_oracle_oic.constants import FlextOracleOicConstants, c
+    from flext_oracle_oic.main import FlextOracleOicCli, main
+    from flext_oracle_oic.models import FlextOracleOicModels, m
+    from flext_oracle_oic.protocols import FlextOracleOicProtocols, p
+    from flext_oracle_oic.service import FlextOracleOicService, s
+    from flext_oracle_oic.services.auth import FlextOracleOicAuthMixin
+    from flext_oracle_oic.services.base import FlextOracleOicServiceBase
     from flext_oracle_oic.services.integration_crud import (
-        FlextOracleOicIntegrationCrudMixin as FlextOracleOicIntegrationCrudMixin,
+        FlextOracleOicIntegrationCrudMixin,
     )
     from flext_oracle_oic.services.integration_lifecycle import (
-        FlextOracleOicIntegrationLifecycleMixin as FlextOracleOicIntegrationLifecycleMixin,
+        FlextOracleOicIntegrationLifecycleMixin,
     )
-    from flext_oracle_oic.services.monitoring import (
-        FlextOracleOicMonitoringMixin as FlextOracleOicMonitoringMixin,
-    )
-    from flext_oracle_oic.services.orchestration import (
-        FlextOracleOicOrchestrationMixin as FlextOracleOicOrchestrationMixin,
-    )
-    from flext_oracle_oic.settings import (
-        FlextOracleOicSettings as FlextOracleOicSettings,
-    )
-    from flext_oracle_oic.typings import (
-        FlextOracleOicTypes as FlextOracleOicTypes,
-        t as t,
-    )
-    from flext_oracle_oic.utilities import (
-        FlextOracleOicUtilities as FlextOracleOicUtilities,
-        u as u,
-    )
-
-
-_LAZY_IMPORTS = FLEXT_ORACLE_OIC_LAZY_IMPORTS
-
-
-_EAGER_EXPORTS = (
-    __author__,
-    __author_email__,
-    __description__,
-    __license__,
-    __title__,
-    __url__,
-    __version__,
-    __version_info__,
+    from flext_oracle_oic.services.monitoring import FlextOracleOicMonitoringMixin
+    from flext_oracle_oic.services.orchestration import FlextOracleOicOrchestrationMixin
+    from flext_oracle_oic.settings import FlextOracleOicSettings
+    from flext_oracle_oic.typings import FlextOracleOicTypes, t
+    from flext_oracle_oic.utilities import FlextOracleOicUtilities, u
+_LAZY_IMPORTS = merge_lazy_imports(
+    (".services",),
+    build_lazy_import_map(
+        {
+            ".api": (
+                "FlextOracleOicApi",
+                "oracle_oic",
+            ),
+            ".constants": (
+                "FlextOracleOicConstants",
+                "c",
+            ),
+            ".main": (
+                "FlextOracleOicCli",
+                "main",
+            ),
+            ".models": (
+                "FlextOracleOicModels",
+                "m",
+            ),
+            ".protocols": (
+                "FlextOracleOicProtocols",
+                "p",
+            ),
+            ".service": (
+                "FlextOracleOicService",
+                "s",
+            ),
+            ".services.auth": ("FlextOracleOicAuthMixin",),
+            ".services.base": ("FlextOracleOicServiceBase",),
+            ".services.integration_crud": ("FlextOracleOicIntegrationCrudMixin",),
+            ".services.integration_lifecycle": (
+                "FlextOracleOicIntegrationLifecycleMixin",
+            ),
+            ".services.monitoring": ("FlextOracleOicMonitoringMixin",),
+            ".services.orchestration": ("FlextOracleOicOrchestrationMixin",),
+            ".settings": ("FlextOracleOicSettings",),
+            ".typings": (
+                "FlextOracleOicTypes",
+                "t",
+            ),
+            ".utilities": (
+                "FlextOracleOicUtilities",
+                "u",
+            ),
+            "flext_auth": (
+                "d",
+                "e",
+                "h",
+                "r",
+                "x",
+            ),
+        },
+    ),
+    exclude_names=(
+        "cleanup_submodule_namespace",
+        "install_lazy_exports",
+        "lazy_getattr",
+        "logger",
+        "merge_lazy_imports",
+        "output",
+        "output_reporting",
+        "pytest_addoption",
+        "pytest_collect_file",
+        "pytest_collection_modifyitems",
+        "pytest_configure",
+        "pytest_runtest_setup",
+        "pytest_runtest_teardown",
+        "pytest_sessionfinish",
+        "pytest_sessionstart",
+        "pytest_terminal_summary",
+        "pytest_warning_recorded",
+    ),
+    module_name=__name__,
 )
 
-
-_PUBLIC_EXPORTS: tuple[str, ...] = (
-    "FlextOracleOicApi",
-    "FlextOracleOicAuthMixin",
-    "FlextOracleOicCli",
-    "FlextOracleOicConstants",
-    "FlextOracleOicIntegrationCrudMixin",
-    "FlextOracleOicIntegrationLifecycleMixin",
-    "FlextOracleOicModels",
-    "FlextOracleOicMonitoringMixin",
-    "FlextOracleOicOrchestrationMixin",
-    "FlextOracleOicProtocols",
-    "FlextOracleOicService",
-    "FlextOracleOicServiceBase",
-    "FlextOracleOicSettings",
-    "FlextOracleOicTypes",
-    "FlextOracleOicUtilities",
-    "oracle_oic",
-    "__author__",
-    "__author_email__",
-    "__description__",
-    "__license__",
-    "__title__",
-    "__url__",
-    "__version__",
-    "__version_info__",
-    "c",
-    "d",
-    "e",
-    "h",
-    "m",
-    "main",
-    "p",
-    "r",
-    "s",
-    "t",
-    "u",
-    "x",
-)
 
 __all__: tuple[str, ...] = (
     "FlextOracleOicApi",
@@ -174,5 +164,5 @@ install_lazy_exports(
     __name__,
     globals(),
     _LAZY_IMPORTS,
-    public_exports=_PUBLIC_EXPORTS,
+    public_exports=__all__,
 )
